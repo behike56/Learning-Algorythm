34efa8a30133d91049fd22ed01acdb0ab9f081a0
behike56 <living.tech.dancing.code@gmail.com>
2021-10-03 10:10:34 +0900

    refactor Kotlin:BinSearch

--- kotlin/BinSearch.kt
+++ kotlin/BinSearch.kt
@@ -1,44 +1,45 @@
-class BinSearch {
-    fun main() {
-        print("要素数:")
-        val num: Int = readLine()?.toInt() ?: 0
-        val x: IntArray(num)
+fun main() {
+    print("要素数:")
+    val num: Int = readLine()?.toInt() ?: 0
+    val x = IntArray(num)
 
-        println("昇順に入力してください。")
+    println("昇順に入力してください。")
 
-        print("x[0]: ")
-        x[0] = readLine()?.toInt() ?: 0
+    print("x[0]: ")
+    x[0] = readLine()?.toInt() ?: 0
 
-        for (i in 0 until num) {
-            do {
-                print("x[" + i + "]: ")
-                x[i] = readLine()?.toInt() ?: 0
-            } while (x[i] < x[i - 1])
-        }
-
-        print("探す値：")
-        val ky: Int = readLine()?.toInt() ?: 0
-        val idx: Int = binSearch(x, num, ky)
-
-        if (idx == -1)
-            print("その値の要素は存在しません。")
-        else
-            print("その値はx[" + idx + "]にあります。")
-    }
-
-    fun binSearch(a: IntArray, n: Int, key: Int) Int {
-        var pl: Int = 0
-        var pr: Int = n - 1
-
+    for (i in 0 until num) {
         do {
-            var pc: Int = (pl + pr) / 2
-            if (a[pc] == key) return pc
-            if (a[pc] < key)
-                pl = pc + 1
-            if (a[pc] > key)
-                pr = pc + 1
-        } while (pl <= pr)
-
-        return -1
+            print("x[" + i + "]: ")
+            x[i] = readLine()?.toInt() ?: 0
+        } while (x[i] < x[i - 1])
     }
+
+    print("探す値：")
+    val ky: Int = readLine()?.toInt() ?: 0
+    val idx: Int = binSearch(x, num, ky)
+
+    if (idx == -1)
+        print("その値の要素は存在しません。")
+    else
+        print("その値はx[" + idx + "]にあります。")
+}
+
+fun binSearch(a: IntArray, n: Int, key: Int): Int {
+    var pl: Int = 0
+    var pr: Int = n - 1
+
+    do {
+        var pc: Int = (pl + pr) / 2
+        if (a[pc] == key)
+            return pc
+
+        if (a[pc] < key)
+            pl = pc + 1
+
+        if (a[pc] > key)
+            pr = pc + 1
+    } while (pl <= pr)
+
+    return -1
 }
